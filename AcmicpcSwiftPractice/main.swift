ec156b8f86cc9fd936f6f2310cc740d04e317ff3
Justin Dongwook Jung <jungdw0624@gmail.com>
2020-01-19 23:45:50 +0900

    initial commit

---
+++ AcmicpcSwiftPractice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AcmicpcSwiftPractice
+//
+//  Created by justin dongwook Jung on 2020/01/19.
+//  Copyright © 2020 justin dongwook Jung. All rights reserved.
+//
+
+import Foundation
+
+print("Hello World!")
+
