931b4ac59b1e51b8c800a83c48bd90483ae03a6f
Justin Dongwook Jung <jungdw0624@gmail.com>
2020-01-20 00:01:34 +0900

    1000,1001 update!

--- prob1001/main.swift
+++ prob1001/main.swift
@@ -1,11 +1,10 @@
 //
 //  main.swift
-//  prob1000
+//  prob1001
 //
-//  Created by justin dongwook Jung on 2020/01/19.
+//  Created by justin dongwook Jung on 2020/01/20.
 //  Copyright © 2020 justin dongwook Jung. All rights reserved.
 //
-
 import Foundation
 
 var line = readLine()!
@@ -13,4 +12,4 @@ var lineArr = line.components(separatedBy: " ")
 let a = Int(lineArr[0])!
 let b = Int(lineArr[1])!
 
-print(a+b)
+print(a+b)
