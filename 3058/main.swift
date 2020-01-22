5820ef66d195b2c61a3c06adb5bc89441e548461
Justin Dongwook Jung <jungdw0624@gmail.com>
2020-01-23 08:48:23 +0900

    3058 working on

---
+++ 3058/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  3058
+//
+//  Created by justin dongwook Jung on 2020/01/23.
+//  Copyright © 2020 justin dongwook Jung. All rights reserved.
+//
+
+import Foundation
+
+for n in 1...7 {
+    
+}
---
+++ prob3058/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  prob3058
+//
+//  Created by justin dongwook Jung on 2020/01/23.
+//  Copyright © 2020 justin dongwook Jung. All rights reserved.
+//
+
+import Foundation
+
+let testcase: Int = readLine()!
+
+for 0...<testcase {
+    var numbers = readLine()?.components(separatedBy: " ")
+    
+}
