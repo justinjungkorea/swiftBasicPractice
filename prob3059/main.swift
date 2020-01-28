1cf7cbc5b209e2d85170ed40a00255a51c89e297
jungdw0624 <jdw8998@daum.net>
2020-01-28 12:52:02 +0900

    3059 update!

--- prob3059/main.swift
+++ prob3059/main.swift
@@ -10,13 +10,27 @@ import Foundation
 
 var testcase: String = readLine()!
 var t: Int = Int(testcase)!
-var total: Int = 0
+var total: Int = 2015
+var result = [Int]()
+
 
 for i in 0..<t {
     let line = readLine()
-    var sum: UInt8 = 0
+    var sum8: UInt8 = 0
+    var sum: Int = 0
+    var charSet = Set<Character>()
     for char in line! {
-        sum += char.asciiValue!
-    }
-    print(sum)
+        charSet.insert(char)
+    }
+    
+    for char in charSet {
+        sum8 = char.asciiValue!
+        sum += Int(sum8)
+    }
+    sum = total - sum
+    result.insert(sum, at: i)
+}
+
+for ans in result {
+    print(ans)
 }
