47c16e74ed5b3158e23aecb299dee0dceadd1221
Justin Dongwook Jung <jungdw0624@gmail.com>
2020-01-28 20:45:51 +0900

    2455 working on...

---
+++ prob2455/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  prob2455
+//
+//  Created by justin dongwook Jung on 2020/01/28.
+//  Copyright © 2020 justin dongwook Jung. All rights reserved.
+//
+
+import Foundation
+
+var people = [Int, Int]()
+
+for i in 0...3 {
+    var line: String = readLine()!
+    var numbers = line.components(separatedBy: " ")
+    
+}
