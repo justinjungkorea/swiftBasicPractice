a0bcead7336273bccf424978946019658bcd1cb9
Justin Dongwook Jung <jungdw0624@gmail.com>
2020-01-29 08:02:12 +0900

    2455 done!

--- prob2455/main.swift
+++ prob2455/main.swift
@@ -8,10 +8,16 @@
 
 import Foundation
 
-var people = [Int, Int]()
+var people = [Int]()
 
 for i in 0...3 {
-    var line: String = readLine()!
-    var numbers = line.components(separatedBy: " ")
-    
+    let line: String = readLine()!
+    let numbers = line.components(separatedBy: " ")
+    if(i==0) {
+        people.insert(Int(numbers[1])!, at: i)
+    } else {
+        people.insert((people[i-1] - Int(numbers[0])! + Int(numbers[1])!), at: i)
+    }
 }
+
+print(people.max()!)
