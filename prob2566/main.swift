920bd95256a932ab6ccaac308681d137d146f939
jungdw0624 <jdw8998@daum.net>
2020-01-29 09:00:47 +0900

    2566 working on

---
+++ prob2566/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  prob2566
+//
+//  Created by jusitn dongwook Jung on 2020/01/29.
+//  Copyright © 2020 justin dongwook Jung. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
