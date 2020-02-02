5899ee75d0fabeba5a6897081d8a2adce3b80dee
Justin Dongwook Jung <jungdw0624@gmail.com>
2020-02-03 08:24:41 +0900

    working on 3184...

--- prob3184/main.swift
+++ prob3184/main.swift
@@ -13,16 +13,34 @@ let rc = rcLine.components(separatedBy: " ");
 let r = Int(rc[0])
 let c = Int(rc[1])
 var map = [String]()
+var yard: [[Int]] = Array(repeating: Array(repeating: 0, count: c!), count: r!)
+var visted: [[Bool]] = Array(repeating: Array(repeating: false, count: c!), count: r!)
+
+
 
 for i in 0..<r! {
     let mapLine: String = readLine()!
     map.insert(mapLine, at: Int(i))
+    for j in 0..<c!{
+        let temp = map[i].index(map[i].startIndex, offsetBy: j)
+        if(map[i][temp] == "."){
+            yard[i][j] = 0
+        } else if (map[i][temp] == "#"){
+            yard[i][j] = 1
+        } else if (map[i][temp] == "v"){
+            yard[i][j] = 2
+        } else if (map[i][temp] == "o"){
+            yard[i][j] = 3
+        }
+    }
 }
 
 for i in 0..<r! {
     for j in 0..<c! {
-        let temp = map[i].index(map[i].startIndex, offsetBy: j)
-        print(map[i][temp], terminator:" ")
+        if(visted[i][j]){
+            continue
+        }
+        
+        
     }
 }
-
