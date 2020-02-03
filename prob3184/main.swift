c96bb55e92fc20f2f4e9b7eb136ae87e36a5804b
Justin Dongwook Jung <jungdw0624@gmail.com>
2020-02-04 08:23:41 +0900

    still working on 3184...

--- prob3184/main.swift
+++ prob3184/main.swift
@@ -14,7 +14,12 @@ let r = Int(rc[0])
 let c = Int(rc[1])
 var map = [String]()
 var yard: [[Int]] = Array(repeating: Array(repeating: 0, count: c!), count: r!)
-var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: c!), count: r!)
+var visited: [[Int]] = Array(repeating: Array(repeating: 0, count: c!), count: r!)
+
+struct Potato {
+    var x = 0
+    var y = 0
+}
 
 class Queue<T> {
     private var array: [T]
@@ -31,14 +36,18 @@ class Queue<T> {
         array.append(data)
     }
     
-    public func dequeue(){
+    public func dequeue() {
         if isEmpty {
-            return nil
+            return
         } else {
-            return array.removeFirst()
+            array.removeFirst()
+            return
         }
     }
     
+    public func top() -> T {
+        return array.first!
+    }
 }
 
 
@@ -61,15 +70,39 @@ for i in 0..<r! {
     }
 }
 
-for i in 0..<r! {
-    for j in 0..<c! {
-        if(visited[i][j] || yard[i][j] != 2 ){
-            continue
-        }
-        
-        visited[i][j] = true
-        
-        
-    }
+var data = Queue<Potato>()
+var temp = Potato()
+temp.x = 0
+temp.y = 0
+
+data.enqueue(data: temp)
+
+while !data.isEmpty {
+    var x = data.top().x
+    var y = data.top().y
+    
+    
 }
 
+
+//void bfs(){
+//    q.push(make_pair(1,1));
+//    check[1][1] = true;
+//    dis[1][1] = 1;
+//    while(!q.empty()){
+//        int x = q.front().first;
+//        int y = q.front().second;
+//        q.pop();
+//        for(int i=0;i<4;++i){
+//            int nx=x+dx[i];
+//            int ny=y+dy[i];
+//            if( (0<nx) && (nx<=n) && (0<ny) && (ny<=m) ){
+//                if(check[nx][ny]==false && map[nx][ny]==1){
+//                    q.push(make_pair(nx, ny));
+//                    dis[nx][ny]=dis[x][y]+1;
+//                    check[nx][ny] = true;
+//                }
+//            }
+//        }
+//    }
+//}
