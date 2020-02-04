32ab53c17b966adb6caaca75d5667e2ba1e403fd
Justin Dongwook Jung <jungdw0624@gmail.com>
2020-02-05 08:32:54 +0900

    working on 3184...

--- prob3184/main.swift
+++ prob3184/main.swift
@@ -12,9 +12,15 @@ let rcLine: String = readLine()!
 let rc = rcLine.components(separatedBy: " ");
 let r = Int(rc[0])
 let c = Int(rc[1])
+let dx = [0, 0, 1, -1]
+let dy = [1, -1, 0, 0]
+
 var map = [String]()
 var yard: [[Int]] = Array(repeating: Array(repeating: 0, count: c!), count: r!)
 var visited: [[Int]] = Array(repeating: Array(repeating: 0, count: c!), count: r!)
+var wolf: Int = 0
+var sheep: Int = 0
+
 
 struct Potato {
     var x = 0
@@ -50,8 +56,54 @@ class Queue<T> {
     }
 }
 
-
-
+func bfs(x: Int, y: Int) {
+    var nSheep: Int = 0
+    var nWoolf: Int = 0
+    
+    let data = Queue<Potato>()
+    var temp = Potato()
+    temp.x = x
+    temp.y = y
+    
+    data.enqueue(data: temp)
+    visited[x][y] = 1
+    
+    if(yard[x][y] == 3){
+        nSheep += 1
+    } else if(yard[x][y] == 2){
+        nWoolf += 1
+    }
+    
+    while !data.isEmpty {
+        let curX = data.top().x
+        let curY = data.top().y
+        data.dequeue()
+        
+        for i in 0...3 {
+            let nx = curX + dx[i]
+            let ny = curY + dy[i]
+            
+            if( (0 <= nx) && (nx <= r!) && (0 <= ny) && (ny <= c!) && (visited[nx][ny] == 0) && (yard[x][y] != 2) ){
+                visited[nx][ny] = 1
+                if(yard[nx][ny] == 3){
+                    nSheep += 1
+                } else if(yard[nx][ny] == 2){
+                    nWoolf += 1
+                }
+                temp.x = nx
+                temp.y = ny
+                data.enqueue(data: temp)
+            }
+        }
+    }
+    
+    if(nSheep > nWoolf){
+        wolf -= nWoolf
+    } else {
+        sheep -= nSheep
+    }
+    
+}
 
 for i in 0..<r! {
     let mapLine: String = readLine()!
@@ -64,45 +116,23 @@ for i in 0..<r! {
             yard[i][j] = 1
         } else if (map[i][temp] == "v"){
             yard[i][j] = 2
+            wolf += 1
         } else if (map[i][temp] == "o"){
             yard[i][j] = 3
+            sheep += 1
         }
     }
 }
 
-var data = Queue<Potato>()
-var temp = Potato()
-temp.x = 0
-temp.y = 0
-
-data.enqueue(data: temp)
-
-while !data.isEmpty {
-    var x = data.top().x
-    var y = data.top().y
-    
-    
+for i in 0..<r! {
+    for j in 0..<c! {
+        if( ((yard[i][j] == 2) || (yard[i][j] == 3)) && (visited[i][j] == 0) ){
+            bfs(i, j)
+        }
+    }
 }
 
 
-//void bfs(){
-//    q.push(make_pair(1,1));
-//    check[1][1] = true;
-//    dis[1][1] = 1;
-//    while(!q.empty()){
-//        int x = q.front().first;
-//        int y = q.front().second;
-//        q.pop();
-//        for(int i=0;i<4;++i){
-//            int nx=x+dx[i];
-//            int ny=y+dy[i];
-//            if( (0<nx) && (nx<=n) && (0<ny) && (ny<=m) ){
-//                if(check[nx][ny]==false && map[nx][ny]==1){
-//                    q.push(make_pair(nx, ny));
-//                    dis[nx][ny]=dis[x][y]+1;
-//                    check[nx][ny] = true;
-//                }
-//            }
-//        }
-//    }
-//}
+
+
+
