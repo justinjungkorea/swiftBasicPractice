20886aaa2893bab4aa07d3d88fd711f845bf2bbc
Justin Dongwook Jung <jungdw0624@gmail.com>
2020-02-06 08:20:10 +0900

    3449 working on...

---
+++ prob3449/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  prob3449
+//
+//  Created by justin dongwook Jung on 2020/02/06.
+//  Copyright © 2020 justin dongwook Jung. All rights reserved.
+//
+
+import Foundation
+
+//var num1 = "10001"
+//var num2 = Int(num1, radix: 2)!
+
+var tStr: String = readLine()!
+var t: Int = Int(tStr)!
+var ans = [Int]()
+
+for i in 0..<t {
+    var num1: String = readLine()!
+    var num2: String = readLine()!
+}
+
