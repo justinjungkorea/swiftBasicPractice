4406de549fe467e8bb0c70d7a6bc39861f0c7f23
justinjungkorea <jungdw0624@gmail.com>
2020-02-14 13:00:53 +0900

    2422 wooing on...

---
+++ prob2422/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  prob2422
+//
+//  Created by kp_justin on 2020/02/14.
+//  Copyright © 2020 justin dongwook Jung. All rights reserved.
+//
+
+import Foundation
+
+func combination(){
+    
+}
